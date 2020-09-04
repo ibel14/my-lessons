0d181579ebe572a764b8da2143b20320da507d1c
ponaehali <iambelousov@gmail.com>
2020-09-05 01:30:59 +0200

    last changes

--- Practice #29/js/script.js
+++ Practice #29/js/script.js
@@ -3,9 +3,20 @@
 const box = document.getElementById('box'),
       btns = document.getElementsByTagName('button'),
       circles = document.getElementsByClassName('circle'),
-      hearts = document.querySelectorAll('heart'),
+      hearts = document.querySelectorAll('.heart'),
       oneHeart = document.querySelector('.heart');
 
-box.style.backgroundColor = 'blue';
+// box.style.backgroundColor = 'blue';
+
+box.style.cssText = 'background-color: green; width: 500px'; // Благодаря команде можем здесь добавлять данные в css
 
 btns[1].style.borderRadius = '100px';
+circles[1].style.backgroundColor = 'red';
+
+// for (let i=0; i < hearts.length; i++) {
+//     hearts[i].style.backgroundColor = 'gray';
+// }
+
+hearts.forEach(item => {
+    item.style.backgroundColor = 'black';
+});

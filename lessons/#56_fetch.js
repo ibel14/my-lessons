f481ab86155d5a2868c4eadebc054be4eeb3b12e
ponaehali <iambelousov@gmail.com>
2020-11-06 14:44:14 +0200

    #56 fetch API example

---
+++ lessons/#56_fetch.js
@@ -0,0 +1,13 @@
+'use strict'
+
+// Just example
+
+fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: "POST",
+    body: JSON.stringify({name: 'Alex'}),
+    headers: {
+        'Content-type': 'application/json'
+    }
+    })
+    .then(response => response.json())
+    .then(json => console.log(json));

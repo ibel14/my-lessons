e5d5a22d63bbe57bcd3c4c44d7cd20caf6cc12c9
ponaehali <iambelousov@gmail.com>
2020-11-16 22:29:59 +0200

    #65 reg exp

---
+++ lessons/#65_regular_expressions.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// new RegExp('pattern', 'flags');
+// /pattern/f
+
+// const ans = prompt('Введите ваше имя');
+
+// const reg = /n/ig;
+// i
+// g
+// m
+
+// console.log(ans.search(reg));
+// console.log(ans.match(reg));
+
+// const pass = prompt('Password');
+
+// console.log(pass.replace(/\./g, "*"));
+
+// console.log('12-34-56'.replace(/-/g, ':'));

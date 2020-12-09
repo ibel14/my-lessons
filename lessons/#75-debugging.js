dcfa69593238c4c28cf886a68bc419c5917320b3
ponaehali <iambelousov@gmail.com>
2020-12-09 23:48:17 +0200

    #74 & 75

---
+++ lessons/#75-debugging.js
@@ -0,0 +1,11 @@
+'use strict';
+
+try {
+    console.log('Normal');
+    console.log(a);
+    console.log('result');
+} catch(error) {
+    console.log('error');
+} // можно еще использовать finally
+
+console.log('Still normal');

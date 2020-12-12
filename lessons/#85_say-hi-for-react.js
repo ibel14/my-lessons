d95d753af9633438666ae48f8474c4fb6ff5b456
ponaehali <iambelousov@gmail.com>
2020-12-12 16:25:46 +0200

    #85 welcome to react

---
+++ lessons/#85_say-hi-for-react.js
@@ -0,0 +1,70 @@
+let names = ['Ivan', 'Ann', 'Ksenia', 'Voldemart'];
+
+let shortNames = names.filter((name) => { // Применил метод filter
+    return name.length < 5
+});
+
+console.log(shortNames);
+
+// ***
+
+let answers = ['IvAn', 'AnnA', 'HelLo'];
+
+answers = answers.map((item) => item.toLowerCase()); // Применил метод map
+
+console.log(answers);
+
+console.log(`Пользователь ${name}, ${age} лет`);
+
+// ***
+
+function fetchData(data, count = 0) {
+
+    console.log(`Данные ${data} в количестве ${count}`)
+}
+fetchData('something');
+
+// ***
+
+function max (a, b, ...numbers) { // Rest-оператор должен быть в конце и один
+    console.log(numbers);
+}
+
+max(3, 4, 67, 97);
+
+// ***
+
+const arr1 = [1, 2, 5],
+      arr2 = [43, 2, 6];
+
+const res = Math.max(...arr1, ...arr2);
+console.log(res);
+
+// ***
+
+const user = {
+    name: 'default',
+    pass: 'qwerty',
+    rights: 'user'
+};
+
+const admin = {
+    name: 'admin',
+    pass: 'root'
+};
+// const res = Object.assign(user, admin);
+const res = {...user, ...admin};
+console.log(res);
+
+// ***
+
+const x = 25, y = 10;
+const coords = {x, y,
+    calcSq() {
+        console.log(this.x*this.y);
+    }
+}
+
+coords.calcSq();
+
+console.log(coords);
